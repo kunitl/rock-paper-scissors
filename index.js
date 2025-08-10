df18802ab3c0a23f573fdb12c08b264c392631f3
kunitl <kunitl@gmail.com>
2025-08-10 13:19:04 +0800

    feat: add logic to play a single round
    Implement playRound with humanChoice and computerChoice parameters.
    Normalize humanChoice to be case-insensitive.
    Log the round winner and update humanScore or computerScore accordingly.

--- index.js
+++ index.js
@@ -36,22 +36,33 @@ let humanScore = 0;
 let computerScore = 0;
 
 // Step 5: Write the logic to play a single round
-
 // Create a new function named playRound.
-
 // Define two parameters for playRound: humanChoice and computerChoice. Use these two parameters to take the human and computer choices as arguments.
-
 // Make your function’s humanChoice parameter case-insensitive so that players can input “rock”, “ROCK”, “RocK”, or other variations.
-
 // Write the code for your playRound function to console.log a string value representing the round winner, such as: “You lose! Paper beats Rock”.
-
 // Increment the humanScore or computerScore variable based on the round winner.
-
+function playRound(humanChoice, computerChoice) {
+  let capHumanChoice =
+    humanChoice.slice(0, 1).toUpperCase() + humanChoice.slice(1).toLowerCase();
+  if (capHumanChoice === computerChoice) {
+    console.log("It's a tie!");
+  } else if (
+    (capHumanChoice === "Rock" && computerChoice === "Scissors") ||
+    (capHumanChoice === "Scissors" && computerChoice === "Paper") ||
+    (capHumanChoice === "Paper" && computerChoice === "Rock")
+  ) {
+    console.log(`You win! ${capHumanChoice} beats ${computerChoice}!`);
+    humanScore += 1;
+  } else {
+    console.log(`You lose! ${computerChoice} beats ${capHumanChoice}!`);
+    computerScore += 1;
+  }
+  console.log(
+    `Human's Score: ${humanScore}, Computer's Score: ${computerScore}`
+  );
+}
 
 // Step 6: Write the logic to play the entire game
-
 // Create a new function named playGame.
-
 // Move your playRound function and score variables so that they’re declared inside of the new playGame function
-
 // Play 5 rounds by calling playRound 5 times.
